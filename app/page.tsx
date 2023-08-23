846fede5702eef0bc87f64a1dec14dce5e811f36
Thiago Junqueira <thiago@voca.com.br>
2023-08-23 10:33:51 -0300

    Componentizando preview modulos + funcionalidade toggle com illustracao

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 "use client"
 import Image from 'next/image'
-import { ActivitySquare, GaugeCircle, GraduationCap, MessagesSquare, Newspaper } from 'lucide-react';
-
-import { Button } from '@/components/ui/button'
 import { Lato } from 'next/font/google'
 import { ContactForm } from '@/components/ContactForm';
+import { ModulesPreview } from '@/components/ModulesPreview';
 
 const lato = Lato({ weight: ['100', '300', '400', '700', '900'], subsets: ['latin'] })
 export default function Home() {
-
-
   return (
     <div className={lato.className}>
       <div className='grid grid-cols-2 gap-4 justify-self-center items-center align-middle'>
@@ -22,13 +18,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='w-fit items-start flex flex-col gap-4 mt-8'>
-        <Button variant={true ? 'default' : 'ghost'}><MessagesSquare size={16} className='text-slate-200 mr-2' /> VOCA Opinião</Button>
-        <Button variant={'ghost'}><ActivitySquare size={16} className='text-slate-600 mr-2' />VOCA Pulso</Button>
-        <Button variant={'ghost'}><Newspaper size={16} className='text-slate-600 mr-2' />VOCA Integra</Button>
-        <Button variant={'ghost'}><GraduationCap size={16} className='text-slate-600 mr-2' />VOCA Conhecimento</Button>
-        <Button variant={'ghost'}><GaugeCircle size={16} className='text-slate-600 mr-2' />VOCA Desempenho</Button>
-      </div>
+      <ModulesPreview />
     </div>
   )
 }
---
+++ components/ModulesPreview/index.tsx
@@ -0,0 +1,62 @@
+"use client"
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { ActivitySquare, GaugeCircle, GraduationCap, MessagesSquare, Newspaper } from 'lucide-react';
+import Image from "next/image";
+
+export function ModulesPreview() {
+    const [selectedOption, setSelectedOption] = useState(0)
+
+    function handleButtonVariant(index: number) {
+        return selectedOption === index ? 'default' : 'ghost'
+    }
+
+    return (
+        <div className="flex gap-6">
+            <div className='w-fit items-start flex flex-col gap-4 mt-8'>
+                <Button variant={handleButtonVariant(0)} onClick={() => setSelectedOption(0)}>
+                    <MessagesSquare size={16} className={selectedOption == 0 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
+                    VOCA Opinião
+                </Button>
+                <Button variant={handleButtonVariant(1)} onClick={() => setSelectedOption(1)}>
+                    <ActivitySquare size={16} className={selectedOption == 1 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
+                    VOCA Pulso
+                </Button>
+                <Button variant={handleButtonVariant(2)} onClick={() => setSelectedOption(2)}>
+                    <Newspaper size={16} className={selectedOption == 2 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
+                    VOCA Integra
+                </Button>
+                <Button variant={handleButtonVariant(3)} onClick={() => setSelectedOption(3)}>
+                    <GraduationCap size={16} className={selectedOption == 3 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
+                    VOCA Conhecimento
+                </Button>
+                <Button variant={handleButtonVariant(4)} onClick={() => setSelectedOption(4)}>
+                    <GaugeCircle size={16} className={selectedOption == 4 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
+                    VOCA Desempenho
+                </Button>
+            </div>
+
+            <ModuleIllustration index={selectedOption} />
+        </div>
+    )
+}
+
+function ModuleIllustration({ index }: { index: number }) {
+    const config = [
+        {src: '/illustrations/chat.png'},
+        {src: '/illustrations/meeting.png'},
+        {src: '/illustrations/team-work.png'},
+        {src: '/illustrations/chat.png'},
+        {src: '/illustrations/meeting.png'},
+    ]
+    
+    return (
+        <Image 
+            src={config[index].src}
+            alt="Ilustração"
+            width={500}
+            height={300}
+            className="h-96 aspect-square"
+        />
+    )
+}
