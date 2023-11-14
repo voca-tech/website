7c267c9db6b1c46510cc7202f22b6d3284a366e2
Thiago Junqueira <thiago.palermoj@gmail.com>
2023-11-14 18:06:53 -0300

    inicio estrutura de funcionalidades

--- app/sections/Functionalities.tsx
+++ app/sections/Functionalities.tsx
@@ -1,51 +1,121 @@
 "use client"
 import { useState } from "react";
-import { ActivitySquare, GaugeCircle, GraduationCap, MessagesSquare, Newspaper } from 'lucide-react';
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function FunctionalitiesSection() {
-    const [selectedOption, setSelectedOption] = useState(0)
+    const options = [
+        {value: 0, name: 'Termômetro de Humor', color: 'bg-teal-600'},
+        {value: 1, name: 'Voz para os colaboradores', color: 'bg-zinc-600'}
+    ]
 
-    function handleButtonVariant(index: number) {
-        return selectedOption === index ? 'default' : 'ghost'
+    const [selectedOption, setSelectedOption] = useState(options[0])
+
+    function Section() {
+        switch (selectedOption.value) {
+            case 0:
+                return <Thermometer />
+            case 1:
+                return <Opinion />
+            // case 2:
+            //     return <Pulse />
+            // case 3:
+            //     return <SocialNetwork />
+            // case 4:
+            //     return <Knowledge />
+            // case 5:
+            //     return <Performance />
+            default:
+                return <Thermometer />
+        }
     }
 
     return (
-        <div className="bg-[#f6a957]">
-            <div className="py-20 px-6 max-w-7xl m-auto text-center">
-                <h1>Módulos</h1>
-
+        <div className={`${selectedOption.color}`}>
+            <div className="inline-flex">
+                {options.map(option => (
+                    <Button key={option.value} onClick={() => setSelectedOption(option)}>
+                        {option.name}
+                    </Button>
+                ))}
             </div>
+            <Section />
         </div>
-        // <div className="flex flex-col md:flex-row gap-2">
-        //     <div className='w-fit items-start flex flex-row flex-wrap md:flex-col gap-2 md:gap-4'>
-        //         <Button variant={handleButtonVariant(0)} onClick={() => setSelectedOption(0)}>
-        //             <MessagesSquare size={16} className={selectedOption == 0 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
-        //             VOCA Opinião
-        //         </Button>
-        //         <Button variant={handleButtonVariant(1)} onClick={() => setSelectedOption(1)}>
-        //             <ActivitySquare size={16} className={selectedOption == 1 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
-        //             VOCA Pulso
-        //         </Button>
-        //         <Button variant={handleButtonVariant(2)} onClick={() => setSelectedOption(2)}>
-        //             <Newspaper size={16} className={selectedOption == 2 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
-        //             VOCA Integra
-        //         </Button>
-        //         <Button variant={handleButtonVariant(3)} onClick={() => setSelectedOption(3)}>
-        //             <GraduationCap size={16} className={selectedOption == 3 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
-        //             VOCA Conhecimento
-        //         </Button>
-        //         <Button variant={handleButtonVariant(4)} onClick={() => setSelectedOption(4)}>
-        //             <GaugeCircle size={16} className={selectedOption == 4 ? 'text-slate-200 mr-2' : 'text-slate-600 mr-2'} />
-        //             VOCA Desempenho
-        //         </Button>
-        //     </div>
-
-        //     <ModuleIllustration index={selectedOption} />
-        // </div>
     )
 }
+
+interface DefaultLayoutProps {
+    Image: JSX.Element;
+    title: string;
+    children: React.ReactNode;
+}
+
+function DefaultLayout({ Image, title, children }: DefaultLayoutProps) {
+    return (
+        <div className="py-8 px-6 max-w-7xl m-auto grid grid-cols-1 md:grid-cols-2">
+            {Image}
+            <div className="text-white">
+                <h1 className="text-5xl">{title}</h1>
+                <div className="mt-8">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Thermometer() {
+    return (
+        <DefaultLayout
+            Image={<Image src="/illustrations/functionalities/termometro.png" width={500} height={500} alt="Imagem Termômetro" />}
+            title="Termômetro de Humor"
+        >
+            <p>Saiba o humor do seu time</p>
+        </DefaultLayout>
+    )
+}
+
+function Opinion() {
+    return (
+        <DefaultLayout
+            Image={<Image src="/illustrations/functionalities/opiniao.png" width={500} height={500} alt="Imagem Opinião" />}
+            title="VOCA Opinião"
+        >
+            <p>5 canais customizados</p>
+            <p>Incentive a cultura de feedback</p>
+            <p>Reconheça as pessoas do seu time</p>
+        </DefaultLayout>
+    )
+}
+
+// function Pulse() {
+//     return (
+//         <DefaultLayout customStyles="bg-zinc-600" />
+//     )
+// }
+
+// function SocialNetwork() {
+//     return (
+//         <DefaultLayout customStyles="bg-orange-600" />
+//     )
+// }
+
+// function Knowledge() {
+//     return (
+//         <DefaultLayout customStyles="bg-purple-600" />
+//     )
+// }
+
+// function Performance() {
+//     return (
+//         <DefaultLayout customStyles="bg-yellow-600" />
+//     )
+// }
+
+
+
+
+
 
 function ModuleIllustration({ index }: { index: number }) {
     const config = [
