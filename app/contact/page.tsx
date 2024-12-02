0973a541d1663abe4dbc9e93692dc26a4a45f185
thiago <thiago@voca.com.br>
2024-12-02 17:53:43 -0300

    pagina de contato e customização no componente contactForm

---
+++ app/contact/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { ContactForm } from "@/components/ContactForm";
+import { useSearchParams } from 'next/navigation'
+
+export default function Contact() {
+    const searchParams = useSearchParams()
+    const source = searchParams.get('source')
+
+    return (
+        <div className="bg-voca-green text-white">
+            <div className="max-w-2xl m-auto py-12 px-4">
+                {source === 'get-demo' && (
+                    <div className="text-center mb-4">
+                        <p className="text-2xl font-bold">Obrigado por concluir a demo do sistema!</p>
+                        <p className="text-sm text-slate-300">Para conhecer mais sobre o produto e suas funcionalidades, preencha o formulário abaixo e entraremos em contato o mais breve possível! 😉</p>
+                    </div>
+                )}
+
+                <ContactForm 
+                    title="Falar com um especialista"
+                    description="Descubra como o VOCA encaixa no dia a dia da sua empresa!"
+                    buttonText="Enviar"
+                />
+            </div>
+        </div>
+    )
+}
