142e25c386a06421af1e82fbbfc4d903aaabb64b
Antonio <acgomesn@gmail.com>
2025-05-07 11:07:33 -0300

    refatorando lógica de redirecionamento e melhorando a interface da página de redirecionamento para o aplicativo VOCA

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -10,6 +10,7 @@ export default function Redirect() {
   const [isMobile, setIsMobile] = useState(true)
   const [playStoreUrl, setPlayStoreUrl] = useState('https://play.google.com/store/apps/details?id=voca.app');
   const [appStoreUrl, setAppStoreUrl] = useState('https://apps.apple.com/br/app/voca/id1438359659');
+  let destinationUrl = "https://www.voca.com.br/app";
 
   useEffect(() => {
     // Função para detectar o sistema operacional
@@ -28,77 +29,69 @@ export default function Redirect() {
     };
 
     const os = getMobileOS();
-    let destinationUrl = "https://www.voca.com.br/app";
-
     // URLs para cada plataforma
     if (os === 'web') {
       setIsMobile(false)
     } else if (os === 'Android') {
-      // Tenta abrir o app com scheme primeiro
-      destinationUrl = `voca://`;
-
-      // Após um timeout, redireciona para Google Play se o app não abrir
-      setTimeout(() => {
-        window.location.href = playStoreUrl;
-      }, 1000);
+      destinationUrl = `voca.app://`;
     } else if (os === 'iOS') {
-      // Tenta abrir o app com scheme primeiro
-      destinationUrl = `voca://`;
-
-      // Após um timeout, redireciona para App Store se o app não abrir
-      setTimeout(() => {
-        // URL para produção
-        window.location.href = appStoreUrl;
-      }, 1000);
+      destinationUrl = `com.vijja.voca.app://`;
     }
 
     if (os !== 'web') {
       window.location.href = destinationUrl;
     }
-  }, [playStoreUrl, appStoreUrl]);
+  }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen p-4 text-center">
+    <div className="flex flex-col items-center justify-center  p-4 text-center">
       {isMobile ? (
-        <>
+        <div className='flex flex-col items-center justify-center mb-8'>
           <h1 className="text-2xl font-bold mb-4">Redirecionando...</h1>
-          <p className="mb-8">Você será redirecionado para o aplicativo VOCA em instantes.</p>
-          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-teal-500"></div>
-        </>
-      ) : (
-        <>
+          <p className="mb-1">Você será redirecionado para o aplicativo VOCA em instantes.</p>
+          <p className="text-sm mb-1">Caso não for redirecionado, clique no botão abaixo para abrir o aplicativo</p>
+          <button
+            onClick={() => { window.location.href = destinationUrl }}
+            className=" bg-voca-green hover:bg-teal-600 text-white font-bold py-3 px-6 rounded-lg transition-all shadow-md hover:shadow-lg"
+          >
+            Abrir VOCA
+          </button>
+        </div>
+      )
+        : (
           <h1 className="text-2xl font-bold mb-4">Este link é exclusivo para uso em celulares</h1>
-          <p className="mb-8">Baixe o aplicativo VOCA em seu dispositivo móvel:</p>
-          <div className="flex flex-row items-center space-x-4 justify-center">
-            <a
-              href={appStoreUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="transition-transform hover:scale-105"
-            >
-              <Image
-                src="/images/app-store-badge.png"
-                alt="Disponível na App Store"
-                width={120}
-                height={40}
-              />
-            </a>
-            <a
-              href={playStoreUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="transition-transform hover:scale-105"
-            >
-              <Image
-                src="/images/google-play-badge.png"
-                alt="Disponível no Google Play"
-                width={135}
-                height={50}
-              />
-            </a>
-          </div>
-        </>
-      )}
+        )
+      }
+
+      <p className="mb-8">Baixe o aplicativo VOCA em seu dispositivo móvel:</p>
+      <div className="flex flex-row items-center space-x-4 justify-center">
+        <a
+          href={appStoreUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="transition-transform hover:scale-105"
+        >
+          <Image
+            src="/images/app-store-badge.png"
+            alt="Disponível na App Store"
+            width={120}
+            height={50}
+          />
+        </a>
+        <a
+          href={playStoreUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="transition-transform hover:scale-105"
+        >
+          <Image
+            src="/images/google-play-badge.png"
+            alt="Disponível no Google Play"
+            width={120}
+            height={50}
+          />
+        </a>
+      </div>
     </div>
   );
 } 
